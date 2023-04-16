dca70e394fa9b63b2b4e608d74169310254c2c5c
Wynstona Jackreece <wpj@Wynstonas-MacBook-Pro.local>
2023-04-16 15:31:28 -0400

    edited div and script elements in body of index.html and edited title element in head of index.html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home.js/index.js.js";
+import Home from "./components/Home.js";
 
 function App() {
   return (
