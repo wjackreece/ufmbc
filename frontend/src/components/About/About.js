f96d0f3046f7f29a3716021b162a2817e1d60ca4
Wynstona Jackreece <wpj@Wynstonas-MacBook-Pro.local>
2023-04-27 17:09:46 -0400

    Edited About component

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -9,7 +9,27 @@ const About = () => {
         <div className={css.about}>
           About Unity Faith Missionary Baptist Church
         </div>
-        <section className={css.mission}>Mission Statement</section>
+        <div className={css.churchStatements}>
+          <div className={css.purpose}>
+            <section className={css.purposeTitle}> Purpose Statement</section>
+            <section className={css.purposeInfo}>
+              To Glorify God by Obey the Great Commandment and Fulfilling the
+              Great Commission
+            </section>
+          </div>
+          <div className={css.mission}>
+            <section className={css.missionTitle}> Mission Statement</section>
+            <section className={css.missionInfo}>
+              To Spread the Gospel of our Lord and Savior Jesus Christ
+              throughout the World, Winning Souls for Christ and Establishing
+              Disciples for the Kingdom of God.
+            </section>
+          </div>
+        </div>
+        <div className={css.resources}>
+          Looking to grow your knowledge and love for God?
+          <button>View our resources</button>
+        </div>
       </div>
     </div>
   );
