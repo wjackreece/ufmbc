a27dc92f7c6ca253b2ff7f173d245ae990ea792e
Wynstona Jackreece <wjackreece@gmail.com>
2023-04-30 17:12:14 -0400

    Edited About component Route to App.js for Home component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="*" element={<Home />} />
+        {/* <Route path="about" element={<About />} /> */}
       </Routes>
       <Footer />
     </BrowserRouter>
