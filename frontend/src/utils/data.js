e59bf6826b6e46448a9fd5cf565a7cfe8205a006
Wynstona Jackreece <wjackreece@gmail.com>
2023-05-09 13:34:50 -0400

    Added photos/buttons to Gallery component and styled

--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -40,8 +40,63 @@ export const ministryInfo = [
 
 export const galleryImages = [
   {
-    id: "",
-    imageName: "",
-    tag: "",
+    id: "1",
+    imageName: "img1.jpg",
+    tag: "mlk",
+  },
+  {
+    id: "2",
+    imageName: "img2.jpg",
+    tag: "mlk",
+  },
+  {
+    id: "3",
+    imageName: "img3.jpg",
+    tag: "mlk",
+  },
+  {
+    id: "4",
+    imageName: "img4.jpg",
+    tag: "mlk",
+  },
+  {
+    id: "5",
+    imageName: "img5.jpg",
+    tag: "mlk2",
+  },
+  {
+    id: "6",
+    imageName: "img6.jpg",
+    tag: "mlk2",
+  },
+  {
+    id: "7",
+    imageName: "img7.jpg",
+    tag: "mlk2",
+  },
+  {
+    id: "8",
+    imageName: "img8.jpg",
+    tag: "mlk2",
+  },
+  {
+    id: "9",
+    imageName: "img9.jpg",
+    tag: "mlk3",
+  },
+  {
+    id: "10",
+    imageName: "img10.jpg",
+    tag: "mlk3",
+  },
+  {
+    id: "11",
+    imageName: "img11.jpg",
+    tag: "mlk3",
+  },
+  {
+    id: "12",
+    imageName: "img12.jpg",
+    tag: "mlk3",
   },
 ];
